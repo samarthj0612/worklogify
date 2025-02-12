4a1bd1d9d487b072eec37386e0a518d8624cdda0
Samarth Jain <samarthj0612@gmail.com>
2025-02-13 00:17:03 +0530

    Refactor Tasks screen to use React Query for task fetching and management; implement task addition and completion toggling with mutations

--- src/screens/main/Tasks.tsx
+++ src/screens/main/Tasks.tsx
@@ -11,7 +11,7 @@ import {
 } from "react-native";
 import { Button, Checkbox } from "react-native-paper";
 import { AntDesign, Ionicons } from "@expo/vector-icons";
-import { collection, doc, getDocs, setDoc, updateDoc } from "firebase/firestore";
+import { collection, doc, getDocs, query, setDoc, updateDoc, where } from "firebase/firestore";
 
 import Divider from "../../components/Divider";
 import CustomModal from "../../components/CustomModal";
@@ -20,16 +20,22 @@ import { useAuth } from "../../context/AuthContext";
 import { db } from "../../firebase/config";
 import { TaskSchema } from "../../types";
 import { createActivityLog } from "../../utils/activity";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const TasksScreen = () => {
   const [isModalVisible, setModalVisible] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("")
-  const { user } = useAuth();
-
   const [title, setTitle] = useState<string>("")
   const [description, setDescription] = useState<string>("")
-  const [tasks, setTasks] = useState<TaskSchema[]>([]);
+  
+  const { user, fetchUserDetails } = useAuth();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (!user) {
+      fetchUserDetails();
+    }
+  }, [user]);
 
   const clearFields = () => {
     setTitle("");
@@ -39,9 +45,11 @@ const TasksScreen = () => {
   }
 
   const fetchTasks = async () => {
-    setIsLoading(true);
+    if(!user?.id) return [];
+
     try {
-      const querySnapshot = await getDocs(collection(db, "tasks"));
+      const q = query(collection(db, "tasks"), where("userId", "==", user.id));
+      const querySnapshot = await getDocs(q);
       const fetchedTasks = querySnapshot.docs.map(doc => {
         const data = doc.data();
         return {
@@ -53,31 +61,31 @@ const TasksScreen = () => {
           createdAt: data.createdAt,
         } as TaskSchema;
       });
-
-      if(fetchedTasks){
-        setTasks(fetchedTasks);
-      }
-      setIsLoading(false);
+      
+      return fetchedTasks;
     } catch (error) {
       console.error("Error fetching tasks:", error);
-      setIsLoading(false);
+      return [];
     }
   };
+  
+  // Fetch Tasks using useQuery
+  const { data: tasks, isLoading, refetch } = useQuery({
+    initialData: [],
+    queryKey: ["tasks"],
+    queryFn: fetchTasks,
+  });
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+  // Mutation to Add Task
+  const { mutate: addTaskMutation, isPending: isTaskAdded} = useMutation({
+    mutationFn: async () => {
+      if (!user?.id) {
+        throw new Error("UserId not found!");
+      }
+      if (!title || !description) {
+        throw new Error("Mandatory params missing!");
+      }
 
-  const addTaskHandler = async () => {
-    setIsLoading(true);
-    
-    if (!user || !user.id || !title || !description) {
-      setError("Mandatory params missing!");
-      setIsLoading(false);
-      return;
-    }
-
-    try {
       const docRef = doc(collection(db, "tasks"));
       const docData = {
         id: docRef.id,
@@ -87,48 +95,52 @@ const TasksScreen = () => {
         status: false,
         createdAt: new Date().toISOString(),
       };
+
       await setDoc(docRef, docData);
-
-      setTasks([docData, ...tasks])
-      setIsLoading(false);
-      clearFields();
-      Alert.alert("Task added successfully with ID:", docRef.id);
-      createActivityLog("Added task with ID: " + docRef.id, "add-task", user.id );
-    } catch (error) {
-      console.error("Error adding task: ", error);
-      setError("Something went wrong");
-      setIsLoading(false);
-    }
-  };
-
-  const toggleTaskCompletion = async (taskId: string, isCompleted: boolean) => {
-    setIsLoading(true);
-    try {
-        await updateDoc(doc(db, "tasks", taskId), {
-            status: !isCompleted
-        });
-
-        setTasks((prevTasks) =>
-            prevTasks.map((task) =>
-                task.id === taskId ? { ...task, status: !isCompleted } : task
-            )
-        );
-      if(user && user.id){
-        createActivityLog("Updated status of task with ID: " + taskId, "list-status", user.id );
+      createActivityLog("Added task with ID: " + docRef.id, "add-task", user.id);
+      return docData;
+    },
+    onSuccess: (newTask) => {
+      queryClient.setQueryData(["tasks"], (oldTasks: TaskSchema[] = []) => [
+        newTask,
+        ...oldTasks,
+      ]);
+      Alert.alert("Task added successfully with ID: " + newTask.id);
+      if (user?.id) {
+        queryClient.invalidateQueries({ queryKey: ["tasksCount", user.id] });
+        queryClient.invalidateQueries({ queryKey: ["activities", user.id] });
       }
-    } catch (error) {
-        console.error("Error updating task status:", error);
-    } finally {
-        setIsLoading(false);
-    }
-  };
+      clearFields();
+    },
+    onError: (error) => setError(error.message),
+  });
+
+  // Mutation to Toggle Task Completion
+  const { mutate: toggleTaskMutation, isPending: isStatusUpdated} = useMutation({
+    mutationFn: async ({ taskId, isCompleted }: { taskId: string; isCompleted: boolean }) => {
+      if(!user?.id) return;
+      await updateDoc(doc(db, "tasks", taskId), { status: !isCompleted });
+      createActivityLog("Updated status of task with ID: " + taskId, "list-status", user?.id);
+    },
+    onSuccess: (_, { taskId, isCompleted }) => {
+      queryClient.setQueryData(["tasks"], (oldTasks: TaskSchema[] = []) =>
+        oldTasks.map((task) =>
+          task.id === taskId ? { ...task, status: !isCompleted } : task
+        )
+      );
+      if (user?.id) {
+        queryClient.invalidateQueries({ queryKey: ["tasksCount", user.id] });
+        queryClient.invalidateQueries({ queryKey: ["activities", user.id] });
+      }
+    },
+  });
   
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
         <Text style={styles.taskToday}>Tasks Today</Text>
-        { isLoading ? <ActivityIndicator size="small" /> : null }
-        <TouchableOpacity onPress={fetchTasks}>
+        { isLoading || isStatusUpdated ? <ActivityIndicator size="small" /> : null }
+        <TouchableOpacity onPress={() => refetch()}>
           <Ionicons name="refresh" size={24} />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => setModalVisible(true)}>
@@ -147,7 +159,7 @@ const TasksScreen = () => {
                   <Text style={styles.taskTitle}>{task.title}</Text>
                   <Text>{task.description}</Text>
                 </View>
-                <Checkbox onPress={() => toggleTaskCompletion(task.id, task.status)} status={task.status ? "checked" : "unchecked"} />
+                <Checkbox onPress={() => toggleTaskMutation({ taskId: task.id, isCompleted: task.status })} status={task.status ? "checked" : "unchecked"} />
               </View>
             );
           })}
@@ -179,7 +191,7 @@ const TasksScreen = () => {
           <Button style={{ ...styles.modalActionButton, ...styles.buttonText }} buttonColor="#ccc" onPress={clearFields} >
             Cancel
           </Button>
-          <Button loading={isLoading} style={{ ...styles.modalActionButton, ...styles.addButton }} onPress={addTaskHandler} disabled={!title.trim()} labelStyle={styles.buttonText} >
+          <Button loading={isTaskAdded} style={{ ...styles.modalActionButton, ...styles.addButton }} onPress={() => addTaskMutation()} labelStyle={styles.buttonText} >
             Add
           </Button>
         </View>
